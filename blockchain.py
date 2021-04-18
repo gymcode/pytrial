549da09ceca2f838be921b435872e60044b3ebc8
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-18 16:45:33 +0000

    add value

--- blockchain.py
+++ blockchain.py
@@ -4,11 +4,11 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value_to_transaction(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value_to_transaction(transaction_amount, last_transaction=get_last_blockchain_value()):
+    blockchain.append([last_transaction, transaction_amount])
 
 add_value_to_transaction(2)
-add_value_to_transaction(3)
-add_value_to_transaction(4)
+
+
 
 print(blockchain)
